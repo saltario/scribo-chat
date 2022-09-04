4dd01ac041f9ade1fe87872e0183a8881349d524
Vadim Shaulskiy <anarant.75@gmail.com>
2022-09-04 17:05:56 +0300

    Update dependencies
    + Add MaterialDrawer
    + Update Kotlin version
    + Update other dependencies

--- app/src/main/java/com/saltario/scribo/MainActivity.kt
+++ app/src/main/java/com/saltario/scribo/MainActivity.kt
@@ -2,8 +2,12 @@ package com.saltario.scribo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.saltario.scribo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
