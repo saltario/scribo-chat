4f5a638380d8befbb76f8cf65815b53ef9f3a449
Vadim Shaulskiy <anarant.75@gmail.com>
2022-09-05 22:38:08 +0300

    Add action menu to settings
    + Refactor resources

--- app/src/main/java/com/saltario/scribo/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/saltario/scribo/ui/fragments/SettingsFragment.kt
@@ -1,10 +1,17 @@
 package com.saltario.scribo.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.saltario.scribo.R
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
     }
 }
