5aad988eabc79eaa28eefde9b2a16965f6bc0e6e
Vadim Shaulskiy <anarant.75@gmail.com>
2022-09-08 20:52:45 +0300

    Update user data in settings

--- app/src/main/java/com/saltario/scribo/ui/fragments/ChangeNameFragment.kt
+++ app/src/main/java/com/saltario/scribo/ui/fragments/ChangeNameFragment.kt
@@ -7,7 +7,7 @@ import com.saltario.scribo.R
 import com.saltario.scribo.utilits.*
 import kotlinx.android.synthetic.main.fragment_change_name.*
 
-class ChangeNameFragment : Fragment(R.layout.fragment_change_name) {
+class ChangeNameFragment : BaseFragment(R.layout.fragment_change_name) {
 
     override fun onResume() {
         super.onResume()
--- app/src/main/java/com/saltario/scribo/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/saltario/scribo/ui/fragments/SettingsFragment.kt
@@ -7,14 +7,25 @@ import com.saltario.scribo.MainActivity
 import com.saltario.scribo.R
 import com.saltario.scribo.activities.RegisterActivity
 import com.saltario.scribo.utilits.AUTH
+import com.saltario.scribo.utilits.USER
 import com.saltario.scribo.utilits.replaceActivity
 import com.saltario.scribo.utilits.replaceFragment
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
         setHasOptionsMenu(true)
+        initFields()
+    }
+
+    private fun initFields() {
+        settings_bio.text = USER.bio
+        settings_fullname.text = USER.fullname
+        settings_phone_number.text = USER.phone
+        settings_status.text = USER.status
+        settings_username.text = USER.username
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
