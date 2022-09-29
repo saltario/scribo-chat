1e6bbdfe7cae236269595687e3f13ec057e668f0
Vadim Shaulskiy <anarant.75@gmail.com>
2022-09-29 17:52:21 +0300

    Add login fragment layout

---
+++ app/src/main/java/com/saltario/scribo/ui/screens/auth/WelcomeFragment.kt
@@ -0,0 +1,8 @@
+package com.saltario.scribo.ui.screens.auth
+
+import androidx.fragment.app.Fragment
+import com.saltario.scribo.R
+
+class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+
+}
