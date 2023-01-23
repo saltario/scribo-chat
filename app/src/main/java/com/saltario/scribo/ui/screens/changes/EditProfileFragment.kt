0e0099f10f8b9bdaf82738fa10d394bc227468c3
Vadim Shaulskiy <anarant.75@gmail.com>
2023-01-23 17:32:12 +0300

    Разметка экрана редактирования профиля

---
+++ app/src/main/java/com/saltario/scribo/ui/screens/changes/EditProfileFragment.kt
@@ -0,0 +1,22 @@
+package com.saltario.scribo.ui.screens.changes
+
+import com.saltario.scribo.R
+import com.saltario.scribo.database.USER
+import com.saltario.scribo.database.setBioToDatabase
+import kotlinx.android.synthetic.main.fragment_change_bio.*
+
+class EditProfileFragment : BaseChangeFragment(R.layout.fragment_edit_profile) {
+
+    override fun onResume() {
+        super.onResume()
+
+        settings_input_bio.setText(USER.bio)
+    }
+
+    override fun change() {
+        super.change()
+
+        val newBio = settings_input_bio.text.toString()
+        setBioToDatabase(newBio)
+    }
+}
