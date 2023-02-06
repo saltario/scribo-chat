8061ddffce250b7af4db5ed993e458918af5006c
Vadim Shaulskiy <anarant.75@gmail.com>
2023-02-06 16:40:07 +0300

    Удаление старого и не нужного

--- app/src/main/java/com/saltario/scribo/ui/screens/auth/WelcomeFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.saltario.scribo.ui.screens.auth
-
-import androidx.fragment.app.Fragment
-import com.saltario.scribo.R
-
-class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
-}
--- app/src/main/java/com/saltario/scribo/ui/screens/changes/EditProfileFragment.kt
+++ app/src/main/java/com/saltario/scribo/ui/screens/changes/EditProfileFragment.kt
@@ -1,13 +1,10 @@
 package com.saltario.scribo.ui.screens.changes
 
 import com.saltario.scribo.R
-import com.saltario.scribo.database.USER
-import com.saltario.scribo.database.setBioToDatabase
+import com.saltario.scribo.ui.screens.BaseFragment
 import com.saltario.scribo.utilits.hideNavBottom
-import kotlinx.android.synthetic.main.fragment_change_bio.*
-import kotlinx.android.synthetic.main.fragment_register.*
 
-class EditProfileFragment : BaseChangeFragment(R.layout.fragment_edit_profile) {
+class EditProfileFragment : BaseFragment(R.layout.fragment_edit_profile) {
 
     override fun onStart() {
         super.onStart()
